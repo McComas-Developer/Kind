9ee5b9a3e747a5416a84fa3297afef90cca31712
Elijah Dangerfield <45648517+Elijah-Dangerfield@users.noreply.github.com>
2019-10-08 17:09:56 -0500

    Base adds repository (#2)
    * adds basic interface for methods in repostiory
    
    * removes createPost
    
    * filles out minimal repository
    
    * adds koin
    
    * adds more to rep layer
    
    * adds basic functionality to pull data from firestore
    
    * fixes issue with collapsing toolbar going behind status bar
    
    * adds collapsing functionality to posts
    
    * creates grey backgroud and removes corner radius from feed cards

---
+++ app/src/main/java/com/dangerfield/kind/di/Modules.kt
@@ -0,0 +1,16 @@
+package com.dangerfield.kind.di
+
+import com.dangerfield.kind.api.Repository
+import com.dangerfield.kind.feed.FeedViewModel
+import com.google.firebase.firestore.FirebaseFirestore
+import org.koin.android.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+val viewModelModule = module {
+
+//    // singleton of the Feed repository
+//    single { Repository(FirebaseFirestore.getInstance() )}
+//
+//    // create the viewModel with the repository dependency INJECTED :)
+//    viewModel { FeedViewModel(get())}
+}
--- app/src/main/java/com/dangerfield/kind/model/Post.kt
+++ app/src/main/java/com/dangerfield/kind/model/Post.kt
@@ -1,4 +1,22 @@
 package com.dangerfield.kind.model
 
-class Post {
+import android.util.Log
+
+data class Post(
+        var UUID: String = "",
+        var timeStamp: String = "",
+        var tags: List<String> = listOf(),
+        //holds the UUID of all of the likers
+        var hearts:  List<String> = listOf(),
+        //holds the https link to the image
+        var images: List<String> = listOf(),
+        var text: String = "",
+        var posterUUID: String = "",
+        var expandedState: ExpandedState = ExpandedState.NONE
+)
+
+enum class ExpandedState {
+    EXPANDED,
+    COLLAPSED,
+    NONE
 }
