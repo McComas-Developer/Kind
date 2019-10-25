4bb7fa2ba3b8db6cdd39cb325238c714830f067e
Elijah Dangerfield <elijahdangerfield111@gmail.com>
2019-10-24 22:16:14 -0500

    adds base search bar

--- app/src/main/java/com/dangerfield/kind/ui/find/FindFragment.java
+++ app/src/main/java/com/dangerfield/kind/ui/find/FindFragment.java
@@ -5,7 +5,11 @@ import android.graphics.Typeface;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavHost;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -18,14 +22,14 @@ import com.google.android.material.appbar.CollapsingToolbarLayout;
 public class FindFragment extends Fragment {
 
     private CollapsingToolbarLayout collapsing_toolbar;
-
+    private CardView searchButton;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view =  inflater.inflate(R.layout.fragment_find, container, false);
         collapsing_toolbar = view.findViewById(R.id.collapsing_toolbar);
-
+        searchButton = view.findViewById(R.id.search_button);
         return view;
     }
 
@@ -36,5 +40,8 @@ public class FindFragment extends Fragment {
         collapsing_toolbar.setExpandedTitleTypeface(
                 Typeface.create(collapsing_toolbar.getExpandedTitleTypeface(), Typeface.BOLD)
         );
+
+        searchButton.setOnClickListener(view ->
+                Navigation.findNavController(view).navigate(R.id.action_mainFragment_to_searchFragment));
     }
 }
