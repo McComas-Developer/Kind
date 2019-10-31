94a02c86dd1ef2a653f6059f3c2ac64b0b1e9666
Elijah Dangerfield <elijahdangerfield111@gmail.com>
2019-10-30 19:03:04 -0500

    adds default recyler view

--- app/src/main/java/com/dangerfield/kind/ui/find/search/SearchFragment.kt
+++ app/src/main/java/com/dangerfield/kind/ui/find/search/SearchFragment.kt
@@ -1,12 +1,16 @@
-package com.dangerfield.kind
+package com.dangerfield.kind.ui.find
 
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.NavHostFragment
+import com.dangerfield.kind.R
+import com.dangerfield.kind.util.showIFF
 import kotlinx.android.synthetic.main.fragment_search.*
 
 /**
@@ -28,5 +32,17 @@ class SearchFragment : Fragment() {
         btn_back.setOnClickListener {
             NavHostFragment.findNavController(this).popBackStack()
         }
+
+        ib_clear.setOnClickListener { tv_search.text.clear() }
+
+        tv_search.addTextChangedListener(object: TextWatcher{
+            override fun afterTextChanged(p0: Editable?) {}
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                ib_clear.showIFF((p0?.length ?: 0) > 0)
+            }
+        })
     }
 }
---
+++ app/src/main/java/com/dangerfield/kind/ui/find/search/SearchViewModel.kt
@@ -0,0 +1,6 @@
+package com.dangerfield.kind.ui.find
+
+import androidx.lifecycle.ViewModel
+
+class SearchViewModel : ViewModel() {
+}
