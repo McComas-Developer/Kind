8072a7eec50a70e508363a598e8e357975a5bc0b
Elijah Dangerfield <elijahdangerfield111@gmail.com>
2019-11-03 21:23:58 -0600

    adds more fake data

--- app/src/main/java/com/dangerfield/kind/ui/create/CreatePostFragment.kt
+++ app/src/main/java/com/dangerfield/kind/ui/create/CreatePostFragment.kt
@@ -17,9 +17,11 @@ import com.bumptech.glide.Glide
 import com.dangerfield.kind.R
 import com.dangerfield.kind.api.CurrentUser
 import com.dangerfield.kind.api.Resource
+import com.dangerfield.kind.util.FakeDataCreator
 import com.dangerfield.kind.util.addCharacterMax
 import com.dangerfield.kind.util.hideKeyBoardOnPressAway
 import com.dangerfield.kind.util.showIFF
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.fragment_create_post.*
 
 class CreatePostFragment : Fragment() {
@@ -42,7 +44,7 @@ class CreatePostFragment : Fragment() {
 
         btn_cancel.setOnClickListener { navigateBack() }
 
-        btn_post.setOnClickListener { post() }
+        btn_post.setOnClickListener { post()}
 
         tv_post_text.hideKeyBoardOnPressAway()
     }
